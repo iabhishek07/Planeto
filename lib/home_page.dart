840c5bc5b20d1938f477dbc04626ca99dd6845d0
Abhishek Wagh <waghabhi21@gmail.com>
2020-05-21 13:46:49 +0530

    NEW: Add HomePage UI/Layout

---
+++ lib/home_page.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:planeto/swiper_data.dart';
+import 'constants.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [gradientStartColor, gradientEndColor],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.3, 0.7]
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Planeto',
+                      style: GoogleFonts.poppins(
+                        fontSize: 44.0,
+                        fontWeight: FontWeight.w900,
+                        color: titleTextColor,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    DropdownButton(
+                      items: [
+                        DropdownMenuItem(
+                          child: Text(
+                            'Solar System',
+                            style: GoogleFonts.poppins(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.w500,
+                              color: secondaryTextColor,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                      onChanged: (value) {},
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Image.asset(
+                          'assets/drop_down_icon.png',
+                        ),
+                      ),
+                      underline: SizedBox(),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 500.0,
+                padding: const EdgeInsets.only(left: 32.0),
+                child: Swiper(
+                  autoplay: true,
+                  itemCount: planets.length,
+                  itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                  
+                  layout: SwiperLayout.STACK,
+                  pagination: SwiperPagination(
+                    builder: DotSwiperPaginationBuilder(
+                      activeSize: 15.0,
+                      space: 8.0,
+                    ),
+                  ),
+                  itemBuilder: (context, index) {
+                    return Stack(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            SizedBox(height: 100.0),
+                            Card(
+                              elevation: 8.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32),
+                              ),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(32.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(height: 100.0),
+                                    Text(
+                                      planets[index].name,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 44.0,
+                                        fontWeight: FontWeight.w900,
+                                        color: primaryTextColor,
+                                      ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    Text(
+                                      'Solar System',
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 23.0,
+                                        fontWeight: FontWeight.w500,
+                                        color: secondaryTextColor,
+                                      ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    SizedBox(height: 32),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Know More',
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward,
+                                          color: secondaryTextColor,
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Image.asset(planets[index].iconImage),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(36.0),
+          ),
+          color: navigationColor,
+        ),
+        padding: const EdgeInsets.all(24.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Image.asset('assets/menu_icon.png'),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Image.asset('assets/search_icon.png'),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Image.asset('assets/profile_icon.png'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planeto',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
